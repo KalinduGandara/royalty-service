2398d70580a9d49ed4b307db49b36a75cf2922a5
KalinduGandara <kalindugandara16@gmail.com>
2023-11-14 07:34:34 +0530

    initial

---
+++ src/main/java/com/example/royalty/RoyaltyApplication.java
@@ -0,0 +1,13 @@
+package com.example.royalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyaltyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyaltyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/royalty/RoyaltyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.royalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyaltyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
